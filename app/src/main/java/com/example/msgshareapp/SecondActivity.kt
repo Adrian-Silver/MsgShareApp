7077b3dd24a92ccab3110250ec26ddcbbef4af89
Adrian-Silver <adrianallangitau@gmail.com>
2020-08-13 18:27:43 +0300

    Implicit-Intent

--- app/src/main/java/com/example/msgshareapp/SecondActivity.kt
+++ app/src/main/java/com/example/msgshareapp/SecondActivity.kt
@@ -2,10 +2,20 @@ package com.example.msgshareapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val bundle: Bundle? = intent.extras
+        val msg = bundle!!.getString("user_message")
+
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+
+        txvUserMessage.text = msg
+
     }
 }
